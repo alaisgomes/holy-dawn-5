152beeb789f01f16242160c0efb4ed89cd7c310f
django <django@eab8bb795f5f>
2024-07-01 14:08:49 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "holy-dawn-5",
+  url: "https://holy-dawn-5.botics.co",
+  api: "https://holy-dawn-5.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
